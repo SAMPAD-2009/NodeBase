d5f0211afad3156545b942d2b3f1e11c385ac406
SAMPAD <scscs5982@gmail.com>
2025-11-19 14:55:41 +0530

    Theme and styling

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -3,11 +3,8 @@ import { requireUnauth } from "@/lib/auth-utils";
 
 const page=async()=>{
     await requireUnauth();
-    return(
-        <div>
-            <RegisterForm />
-        </div>
-    )
-};
+    return <RegisterForm />;
+}
+   
 
 export default page;
