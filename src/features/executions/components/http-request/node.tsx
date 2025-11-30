caac52fa371cf2d4b8414bc166f5215b6b400d21
SAMPAD <scscs5982@gmail.com>
2025-11-30 21:50:13 +0530

    Node Selector

---
+++ src/features/executions/components/http-request/node.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { BaseExecutionNode } from "../base-execution-node";
+import { Node, NodeProps, useReactFlow } from "@xyflow/react";
+import { GlobeIcon } from "lucide-react";
+import { memo, useState } from "react";
+
+type HttpRequestNodeData = {
+    endpoint?: string;
+    method?: "GET" | "POST" | "PUT" | "DELETE";
+    body?: string;
+    [key: string]: unknown;
+};
+
+type HttpRequestNodeType = Node<HttpRequestNodeData>;
+
+export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
+    const nodeData = props.data as HttpRequestNodeData;
+    const description = nodeData?.endpoint
+        ? `${nodeData.method || "GET"} ${nodeData.endpoint}`
+        : "Not Configured";
+
+    return (
+        <>
+            <BaseExecutionNode
+                {...props}
+                id={props.id}
+                icon={GlobeIcon}
+                name="HTTP Request"
+                description={description}
+                onSettings={() => { }}
+                onDoubleClick={() => { }}
+            />
+        </>
+    )
+});
+
+HttpRequestNode.displayName = "HttpRequestNode";
